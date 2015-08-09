ce6c370b27bca9d9af93c4eb5dc8d7a772154365
UclCommander <luaeditor94@gmail.com>
2015-08-09 13:35:43 -0500

    Update version command.

--- commands/version.php
+++ commands/version.php
@@ -11,12 +11,20 @@ use Dan\Irc\Location\User;
 
 if($entry == 'use' || $entry == 'console')
 {
-    message($location, Dan::VERSION);
+    if(!PHAR)
+    {
+        $v = Dan::getCurrentGitVersion();
+       $version = "{yellow}{$v['id']}{reset} | {cyan}{$v['message']}";
+    }
+    else
+        $version = "{cyan} PHAR " . Dan::VERSION;
+    
+    message($location, "{reset}[ {$version} {reset}]");
 }
 
 if($entry == 'help')
 {
     return [
-        "Gets the current version of dan."
+        "Gets the current version of Dan."
     ];
 }
