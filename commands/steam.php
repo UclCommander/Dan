4ec3536bb1ef812a7a41ca1734e73db87b6198a7
UclCommander <luaeditor94@gmail.com>
2015-08-23 15:33:51 -0500

    Update steam command

--- commands/steam.php
+++ commands/steam.php
@@ -13,48 +13,59 @@ if($entry == 'use')
 {
     $data = explode(' ', $message);
 
-    if($data[0] == 'save')
+    switch($data[0])
     {
-        database()->table('users')->where('nick', $user->nick())->update(['info' => ['steam' => $data[1]]]);
-        message($channel, "{reset}[ {yellow}{$data[1]} {reset}is now saved to your nickname. ]");
-    }
-    else
-    {
-        $person = empty($message) ? null : $data[0];
-
-        if($person == null)
+        case 'save':
         {
-            $person = $user->nick();
-
-            $data = database()->table('users')->where('nick', $user->nick())->first();
-
-            if(isset($data['info']))
-                if(isset($data['info']['steam']))
-                    $person = $data['info']['steam'];
+            database()->table('users')->where('nick', $user->nick())->update(['info' => ['steam' => $data[1]]]);
+            message($channel, "{reset}[ {yellow}{$data[1]} {reset}is now saved to your nickname. ]");
+            break;
         }
 
-        $data = Web::json("http://fm1337.com/api/steam/{$person}");
-
-        if($data == null)
+        default:
         {
-            message($channel, "{reset}[ {cyan}Error fetching steam information. {reset}]");
-            return;
+            $person = empty($message) ? null : $data[0];
+
+            if($person == null)
+            {
+                $person = $user->nick();
+
+                $data = database()->table('users')->where('nick', $user->nick())->first();
+
+                if(isset($data['info']))
+                    if(isset($data['info']['steam']))
+                        $person = $data['info']['steam'];
+            }
+
+            $data = Web::api('steam/get', ['user' => $person]);
+
+            if($data == null)
+            {
+                message($channel, "{reset}[ {cyan}Error fetching information. {reset}]");
+                return;
+            }
+
+            if($data['success'] == 42)
+            {
+                message($channel, "{reset}[ {cyan}{$data['message']} {reset}]");
+                return;
+            }
+
+            $games = $data['games']['game_count'];
+            $realname = isset($data['current']['realname']) ? $data['current']['realname'] : $data['current']['personaname'];
+            $level = $data['level'];
+            $played = $data['games']['total_played'];
+            $playtime = $data['games']['total_playtime'];
+            $currentGame = isset($data['current']['gameextrainfo']) ? $data['current']['gameextrainfo'] : null;
+            $online = $data['current']['personastate'] > 0 ? "{green}ONLINE" : "{red}OFFLINE";
+
+            if($currentGame)
+                $currentGame = " {cyan}Currently playing {yellow}{$currentGame} {reset}|";
+
+            message($channel, "{reset}[ {$online} {reset}| {yellow}{$realname} {reset}| {light_cyan}Level {$level} {reset}|{$currentGame} {light_cyan}{$playtime} {cyan}hours on record {reset}| {light_cyan}{$played}{cyan} of {light_cyan}{$games} {cyan}games played {reset}]");
+            break;
         }
 
-        $games = $data['games']['game_count'];
-        $realname = isset($data['current']['realname']) ? $data['current']['realname'] : $data['current']['personaname'];
-        $level = $data['level'];
-        $played = $data['games']['total_played'];
-        $playtime = $data['games']['total_playtime'];
-        $currentGame = isset($data['current']['gameextrainfo']) ? $data['current']['gameextrainfo'] : null;
-        $online = $data['current']['personastate'] > 0 ? "{green}ONLINE" : "{red}OFFLINE";
-
-        if($currentGame)
-        {
-            $currentGame = " {cyan}Currently playing {yellow}{$currentGame} {reset}|";
-        }
-
-        message($channel, "{reset}[ {$online} {reset}| {yellow}{$realname} {reset}| {light_cyan}Level {$level} {reset}|{$currentGame} {light_cyan}{$playtime} {cyan}hours on record {reset}| {light_cyan}{$played}{cyan} of {light_cyan}{$games} {cyan}games played {reset}]");
     }
 }
 
