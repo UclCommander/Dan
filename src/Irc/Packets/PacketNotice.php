4705153c1d65290434a6aab388d70f3ed9c271f7
UclCommander <luaeditor94@gmail.com>
2015-09-20 19:00:42 -0500

    Show notices in console

--- src/Irc/Packets/PacketNotice.php
+++ src/Irc/Packets/PacketNotice.php
@@ -7,6 +7,9 @@ class PacketNotice implements PacketContract {
 
     public function handle(Connection $connection, array $from, array $data)
     {
+        if(!DEBUG)
+            console("[<magenta>{$connection->getName()}</magenta>][<cyan>{$data[0]}</cyan>][<yellow>{$from[0]}</yellow>] ** {$data[1]}");
+
         if($data[0] == $connection->user->nick())
         {
             $user       = user($from);
