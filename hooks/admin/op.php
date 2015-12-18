eaaaec28563f634e1ef7c97afbfc42eab8aff338
UclCommander <luaeditor94@gmail.com>
2015-12-18 15:51:59 -0600

    Add op and deop commands

---
+++ hooks/admin/op.php
@@ -0,0 +1,41 @@
+<?php
+
+use Dan\Irc\Location\Channel;
+use Illuminate\Support\Collection;
+
+hook('op')
+    ->command(['op'])
+    ->console()
+    ->rank('aq')
+    ->help("Ops a user")
+    ->func(function(Collection $args) {
+        /** @var Channel $channel */
+        $channel = $args->get('channel');
+        $message = $args->get('message');
+
+        if(!$channel->hasUser($message)) {
+            $channel->message("That user is not here!");
+            return;
+        }
+
+        $channel->userMode(user($message), '+o');
+    });
+
+
+hook('deop')
+    ->command(['deop'])
+    ->console()
+    ->rank('aq')
+    ->help("De-Ops a user")
+    ->func(function(Collection $args) {
+        /** @var Channel $channel */
+        $channel = $args->get('channel');
+        $message = $args->get('message');
+
+        if(!$channel->hasUser($message)) {
+            $channel->message("That user is not here!");
+            return;
+        }
+
+        $channel->userMode(user($message), '-o');
+    });
