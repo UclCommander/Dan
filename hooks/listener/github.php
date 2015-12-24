1216f47d81aa721ec521bff61eb6d6440dc86e0c
UclCommander <luaeditor94@gmail.com>
2015-12-24 14:41:29 -0600

    Update github hook, new twitter hook

--- hooks/listener/github.php
+++ hooks/listener/github.php
@@ -11,48 +11,75 @@ hook('github_push')
     ->http()
     ->post('/github/push')
     ->func(function(Collection $args) {
-        $data   = $args->get('data');
-        $commit = head($data['commits']);
-        $repo   = $data['repository'];
+        $data    = $args->get('data');
+        $repo    = $data['repository'];
+        $config  = config('web.routes.github_push');
 
-        $config = config('web.routes.github_push');
-
-        if(!array_key_exists($repo['full_name'], $config))
+        if (!array_key_exists($repo['full_name'], $config)) {
             return;
+        }
 
         $config = $config[$repo['full_name']];
 
-        $added      = "+" . count($commit['added']);
-        $removed    = "-" . count($commit['removed']);
-        $changed    = "*" . count($commit['modified']);
+        $message = '';
 
-        $branch = last(explode('/', $data['ref']));
+        if(isset($data['commits'])) {
+            $commit = head($data['commits']);
 
-        $compiled = [
-            "<cyan>{$repo['name']}</cyan>",
-            "<yellow>{$repo['owner']['name']}</yellow>",
-            "<light_cyan>" . cleanString($repo['description']) . "</light_cyan>",
-            "<orange>{$branch}</orange>",
-            "{$repo['language']}",
-            "<cyan>{$commit['author']['name']}</cyan>",
-            "<yellow>" . substr($commit['id'], 0, 8) . "</yellow>",
-            "<light_cyan>{$commit['message']}</light_cyan>",
-            "<green>$added</green>",
-            "<red>$removed</red>",
-            "<orange>$changed</orange>",
-        ];
+            $added = "+".count($commit['added']);
+            $removed = "-".count($commit['removed']);
+            $changed = "*".count($commit['modified']);
 
-        foreach($config['post_to'] as $network => $channel)
-        {
-            if(!Dan::hasConnection($network))
+            $branch = last(explode('/', $data['ref']));
+
+            $compiled = [
+                "[ Github - New Commit ] <cyan>{$repo['full_name']}</cyan>",
+                "<light_cyan>" . cleanString($repo['description']) . "</light_cyan>",
+                "<orange>{$branch}</orange>",
+                "<yellow>" . substr($commit['id'], 0, 8) . "</yellow>",
+                "<cyan>{$commit['author']['name']}</cyan>",
+                "<light_cyan>{$commit['message']}</light_cyan>",
+                "<green>$added</green>/<red>$removed</red>/<orange>$changed</orange>",
+                shortLink($commit['url']),
+            ];
+
+            $message = implode(' - ', array_filter($compiled));
+
+        } elseif (isset($data['issue'])) {
+            $issue = $data['issue'];
+
+            if($data['action'] != 'opened' && $data['action'] != 'closed') {
+                return;
+            }
+
+            $labels = [];
+
+            foreach ($issue['labels'] as $label) {
+                $labels[] = $label['name'];
+            }
+
+            $compiled = [
+                "[ Github - " . ucfirst($data['action']) . " Issue ] <cyan>{$repo['full_name']}</cyan>",
+                "<light_cyan>" . cleanString($repo['description']) . "</light_cyan>",
+                "Created by <cyan>{$issue['assignee']['login']}</cyan>",
+                "<light_cyan>{$issue['title']}</light_cyan>",
+                shortLink($issue['url']),
+            ];
+
+            $message = implode(' - ', array_filter($compiled));
+        }
+
+        foreach ($config['post_to'] as $network => $channel) {
+            if (!Dan::hasConnection($network)) {
                 continue;
+            }
 
             $connection = connection($network);
 
-            if(!$connection->inChannel($channel))
+            if (!$connection->inChannel($channel)) {
                 continue;
+            }
 
-            $connection->getChannel($channel)->message("[ " . implode(' | ', array_filter($compiled)) . " ]");
-            $connection->getChannel($channel)->message("[ {$commit['url']} ]");
+            $connection->getChannel($channel)->message($message);
         }
     });
---
+++ hooks/listener/twitter.php
@@ -0,0 +1,49 @@
+<?php
+
+use Dan\Core\Dan;
+use Endroid\Twitter\Twitter;
+use Illuminate\Support\Collection;
+
+hook('twitter_tweets')
+    ->http()
+    ->get('/twitter/update')
+    ->func(function(Collection $args) {
+        $config = config('web.routes.twitter_tweets');
+
+        $twitter = new Twitter($config['api_key'], $config['api_secret'], $config['access_token'], $config['access_secret']);
+
+        $db = database()->table('cache')->where('key', 'twitter_posted_tweets');
+        $cache = $db->first()->get('value');
+
+        if($db->count() == 0) {
+            $db->insert(['key' => 'twitter_posted_tweets', 'value' => []]);
+            $cache = [];
+        }
+
+        foreach($config['users'] as $user) {
+            $data = $twitter->getTimeline(['screen_name' => $user['name'], 'count' => 1]);
+
+            $data = $data[0];
+
+            if(in_array($data->id_str, $cache)) {
+                continue;
+            }
+
+            if (!Dan::hasConnection($user['network'])) {
+                continue;
+            }
+
+            $connection = connection($user['network']);
+
+            if (!$connection->inChannel($user['channel'])) {
+                continue;
+            }
+
+            $connection->getChannel($user['channel'])
+                ->message("[ Twitter ] <cyan>@{$data->user->screen_name}:</cyan> {$data->text} - " . shortLink("https://twitter.com/{$data->user->screen_name}/status/{$data->id_str}"));
+
+            $cache[] = $data->id_str;
+        }
+
+        $db->update(['value' => $cache]);
+    });
