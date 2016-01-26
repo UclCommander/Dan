77ef68f040f480194066e78e3ee4d6f7a1b2709c
UclCommander <luaeditor94@gmail.com>
2016-01-26 09:40:22 -0600

    Clean up the title fetcher

--- hooks/web/url.php
+++ hooks/web/url.php
@@ -25,57 +25,59 @@ hook('title')
         /** @var Channel $channel */
         $channel = $args->get('channel');
 
-        foreach(array_unique($matches) as $match)
-        {
+        foreach (array_unique($matches) as $match) {
             $url = get_final_url($match);
 
             $info = parse_url($url);
 
-            foreach($ignored as $ignore)
-                if(fnmatch($ignore, $info['host']))
+            foreach ($ignored as $ignore) {
+                if (fnmatch($ignore, $info['host'])) {
                     continue 2;
+                }
+            }
 
-            $headers    = getHeaders($url);
-            $type       = is_array($headers['content-type']) ? reset($headers['content-type']) : $headers['content-type'];
-            $mimeType   = explode(';', $type)[0];
+            $headers = getHeaders($url);
+            $type = is_array($headers['content-type']) ? reset($headers['content-type']) : $headers['content-type'];
+            $mimeType = explode(';', $type)[0];
 
-            if(!in_array($mimeType, $mime))
+            if (!in_array($mimeType, $mime)) {
                 continue;
+            }
 
-            if($mimeType == 'text/html')
-            {
+            if ($mimeType == 'text/html') {
                 $html = Web::get($url);
 
                 preg_match("/\<title.*?\>(.*?)\<\/title\>/i", $html, $title);
 
-                $title = $title[1];
+                $title = $title[1] ?? null;
+
+                if (empty($title)) {
+                    continue;
+                }
+
                 $title = str_replace("\n", '', str_replace("\r", '', $title));
                 $title = preg_replace('([ ]+)', ' ', trim($title));
-
                 $title = html_entity_decode($title, ENT_QUOTES);
 
-                if(empty($title))
-                    continue;
-
                 $channel->message("[ <cyan>{$title}</cyan> ]");
-            }
-            else // assume image
-            {
+            } else {
                 $type = $headers['content-type'];
 
-                if(is_array($type))
+                if (is_array($type)) {
                     $type = reset($type);
+                }
 
-                $size   = isset($headers['content-length']) ? $headers['content-length'] : 0;
-                $img    = getimagesize($match);
-                $rez    = (count($img) > 1 ? "{$img[0]}x{$img[1]}" : '-');
+                $size = isset($headers['content-length']) ? $headers['content-length'] : 0;
+                $img = getimagesize($match);
+                $rez = (count($img) > 1 ? "{$img[0]}x{$img[1]}" : '-');
 
-                if(is_array($size))
+                if (is_array($size)) {
                     $size = reset($size);
+                }
 
                 $size = (!$size ? false : convert($size));
 
-                $channel->message("[ <yellow>{$type}</yellow> |" . ($size ? " <cyan>{$size}</cyan> |" : '') . " <cyan>{$rez}</cyan> ]");
+                $channel->message("[ <yellow>{$type}</yellow> |".($size ? " <cyan>{$size}</cyan> |" : '')." <cyan>{$rez}</cyan> ]");
             }
         }
 
