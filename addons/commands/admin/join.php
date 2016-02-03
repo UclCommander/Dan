4480b1f2ec3054d8e413620bfa48768ecdc58bb1
UclCommander <luaeditor94@gmail.com>
2016-02-03 12:22:20 -0500

    Applied fixes from StyleCI

--- addons/commands/admin/join.php
+++ addons/commands/admin/join.php
@@ -9,19 +9,21 @@ command(['join'])
     ->usableInConsole()
     ->requiresIrcConnection()
     ->rank('ASC')
-    ->helpText("Joins a channel")
+    ->helpText('Joins a channel')
     ->handler(function (Connection $connection, UserContract $user, $message, Channel $channel = null) {
         $location = $channel ?? $user;
 
         if (empty($message)) {
-            $location->message("Please provide a channel.");
+            $location->message('Please provide a channel.');
+
             return;
         }
 
         $data = explode(':', $message);
 
         if (!$connection->isChannel($data[0])) {
-            $location->message("Channel name is invalid.");
+            $location->message('Channel name is invalid.');
+
             return;
         }
 
