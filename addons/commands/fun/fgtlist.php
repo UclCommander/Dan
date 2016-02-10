525e80acbd4aea63be8a74500a9fc67319d74b5e
matthijs00 <matthijs186@live.nl>
2016-02-10 22:37:16 +0100

    Fix fgtlist sending notice to channel instead of always user, when used in channel

--- addons/commands/fun/fgtlist.php
+++ addons/commands/fun/fgtlist.php
@@ -7,22 +7,19 @@
  * If you want to change the rank, see commands.permissions in the configuration.
  */
 
-use Dan\Irc\Location\Channel;
 use Dan\Irc\Location\User;
 
 command(['fgtlist', 'fgts'])
     ->allowPrivate()
     ->helpText('Get da fgts')
-    ->handler(function (User $user, Channel $channel) {
-        $location = $channel ?? $user;
+    ->handler(function (User $user) {
         $list = [
             'Chris',
             'Mirz <3',
-            'RoboDash',
-            'The average 12 years old '
+            'RoboDash'
         ];
 
         foreach($list as $fgt) {
-            $location->notice($fgt);
+            $user->notice($fgt);
         }
     });
