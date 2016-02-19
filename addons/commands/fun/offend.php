96f59c7e122d3411eb6d3e201ac6ca8588535753
matthijs00 <matthijs186@live.nl>
2016-02-19 20:37:41 +0100

    Add offend

---
+++ addons/commands/fun/offend.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Offend command.
+ *
+ * Do not directly edit this file.
+ * If you want to change the rank, see commands.permissions in the configuration.
+ */
+
+use Dan\Irc\Location\Channel;
+use Dan\Irc\Location\User;
+
+command('offend')
+    ->allowPrivate()
+    ->helpText('Offend command')
+    ->handler(function (User $user, Channel $channel = null) {
+        $location = $channel ?? $user;
+
+        $location->message('Hi.');
+    });
