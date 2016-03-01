15f7de8088158c11ec9d3f88b3c781037ec614f8
UclCommander <luaeditor94@gmail.com>
2016-03-01 13:22:41 -0600

    Info command

---
+++ addons/commands/utility/info.php
@@ -0,0 +1,24 @@
+<?php
+
+use Dan\Contracts\UserContract;
+use Dan\Core\Dan;
+use Dan\Irc\Connection;
+use Dan\Irc\Location\Channel;
+
+command(['info'])
+    ->allowPrivate()
+    ->allowConsole()
+    ->helpText('Gives information on the bot.')
+    ->handler(function(UserContract $user, Channel $channel = null, Connection $connection = null) {
+        $location = $channel ?? $user;
+
+        $version = Dan::VERSION;
+
+        $prefix = '/';
+
+        if ($user instanceof \Dan\Irc\Location\User) {
+            $prefix = $connection->config->get('command_prefix');
+        }
+
+        $location->message("Dan the IRC bot v{$version} by UclCommander. https://links.ml/XOH4 - See <b>{$prefix}help</b> for a list of commands.");
+    });
