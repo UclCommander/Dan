fd3f7a32fa2aed98b523c8fd3793a57efb2c4b3a
UclCommander <luaeditor94@gmail.com>
2016-03-01 13:25:02 -0600

    geoip command

---
+++ addons/commands/utility/geoip.php
@@ -0,0 +1,41 @@
+<?php
+
+use Dan\Contracts\UserContract;
+use Dan\Irc\Location\Channel;
+use Dan\Support\Web;
+
+command(['geoip'])
+    ->allowPrivate()
+    ->allowConsole()
+    ->helpText('Gets geographic information for the given IP.')
+    ->handler(function(UserContract $user, $message, Channel $channel = null) {
+        $location = $channel ?? $user;
+
+        if (empty($message)) {
+            $location->message('I need an IP to get information for!');
+            return;
+        }
+
+        $data = Web::json("http://geoip.cf/api/{$message}");
+
+        if ($data == null) {
+            $location->message('Unable to fetch IP information.');
+            return;
+        }
+
+        if (isset($data['success']) && !$data['success']) {
+            $location->message($data['message']);
+            return;
+        }
+
+        $data = [
+            "<cyan>Country:</cyan> <yellow>{$data['country']}</yellow>",
+            "<cyan>Continent:</cyan> <yellow>{$data['continent']}</yellow>",
+            "<cyan>Lat:</cyan> <yellow>{$data['latitude']}</yellow>",
+            "<cyan>Lng:</cyan> <yellow>{$data['longitude']}</yellow>",
+            "<cyan>Accuracy:</cyan> <yellow>{$data['accuracy']}</yellow>",
+        ];
+
+        $info = implode(' | ', $data);
+        $location->message("[ {$info} ]");
+    });
