ed028ba3c8958e1bc932827e6337648e144e6fd0
UclCommander <luaeditor94@gmail.com>
2016-03-08 16:50:49 -0600

    Notice handler

--- src/Irc/Packets/PacketNotice.php
+++ src/Irc/Packets/PacketNotice.php
@@ -2,14 +2,31 @@
 
 namespace Dan\Irc\Packets;
 
+use Dan\Events\Traits\EventTrigger;
+
 class PacketNotice extends Packet
 {
+    use EventTrigger;
+
     /**
      * @param array $from
      * @param array $data
      */
     public function handle(array $from, array $data)
     {
-        // var_dump($from, $data);
+        if (!config('dan.debug')) {
+            console("[<magenta>{$this->connection->getName()}</magenta>][<cyan>{$data[0]}</cyan>][<yellow>".($from[0] ?? null)."</yellow>] ** {$data[1]}");
+        }
+
+        if ($data[0] == $this->connection->user->nick) {
+            $user = $this->makeUser($from);
+            $message = $data[1];
+            
+            $this->triggerEvent('irc.notice.private', [
+                'connection'    => $this->connection,
+                'user'          => $user,
+                'message'       => $message,
+            ]);
+        }
     }
 }
