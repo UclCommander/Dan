45352469730309103a32d5da03fdbafbea5df016
UclCommander <luaeditor94@gmail.com>
2016-04-03 17:03:35 -0500

    Prevent twitter from causing crashes.

--- addons/web/twitter.php
+++ addons/web/twitter.php
@@ -32,49 +32,53 @@ route('twitter.update')
         $twitter = new Twitter($access['api_key'], $access['api_secret'], $access['access_token'], $access['access_secret'], $access['api_url'], $access['proxy'], $access['timeout']);
 
         foreach($users as $user) {
-            $data = $twitter->getTimeline([
-                'screen_name'       => $user['name'],
-                'count'             => 5,
-                'exclude_replies'   => !($user['replies'] ?? true)
-            ]);
+            try {
+                $data = $twitter->getTimeline([
+                    'screen_name' => $user['name'],
+                    'count' => 5,
+                    'exclude_replies' => !($user['replies'] ?? true)
+                ]);
 
-            if (count($data) == 0) {
-                continue;
-            }
-
-            $db = database($user['network'])->table('cache')->where('key', 'twitter_posted_tweets');
-            $cache = $db->first()->get('value');
-
-            if($db->count() == 0) {
-                $db->insert(['key' => 'twitter_posted_tweets', 'value' => []]);
-                $cache = [];
-            }
-
-            foreach ($data as $tweet) {
-                if (in_array($tweet->id_str, $cache)) {
+                if (count($data) == 0) {
                     continue;
                 }
 
-                if (!connection()->hasConnection($user['network'])) {
-                    continue;
+                $db = database($user['network'])->table('cache')->where('key', 'twitter_posted_tweets');
+                $cache = $db->first()->get('value');
+
+                if ($db->count() == 0) {
+                    $db->insert(['key' => 'twitter_posted_tweets', 'value' => []]);
+                    $cache = [];
                 }
 
-                /** @var \Dan\Irc\Connection $connection */
-                $connection = connection($user['network']);
+                foreach ($data as $tweet) {
+                    if (in_array($tweet->id_str, $cache)) {
+                        continue;
+                    }
 
-                if (!$connection->inChannel($user['channel'])) {
-                    continue;
+                    if (!connection()->hasConnection($user['network'])) {
+                        continue;
+                    }
+
+                    /** @var \Dan\Irc\Connection $connection */
+                    $connection = connection($user['network']);
+
+                    if (!$connection->inChannel($user['channel'])) {
+                        continue;
+                    }
+
+                    $text = htmlspecialchars_decode(cleanString($tweet->text), ENT_QUOTES);
+                    $link = shortLink("https://twitter.com/{$tweet->user->screen_name}/status/{$tweet->id_str}");
+
+                    $connection->getChannel($user['channel'])
+                        ->message("[ Twitter ] <cyan>@{$tweet->user->screen_name}:</cyan> {$text} - {$link}");
+
+                    $cache[] = $tweet->id_str;
                 }
 
-                $text = htmlspecialchars_decode(cleanString($tweet->text), ENT_QUOTES);
-                $link = shortLink("https://twitter.com/{$tweet->user->screen_name}/status/{$tweet->id_str}");
-
-                $connection->getChannel($user['channel'])
-                    ->message("[ Twitter ] <cyan>@{$tweet->user->screen_name}:</cyan> {$text} - {$link}");
-
-                $cache[] = $tweet->id_str;
+                $db->update(['value' => $cache]);
+            } catch (\Exception $e) {
+                console()->exception($e);
             }
-
-            $db->update(['value' => $cache]);
         }
     });
