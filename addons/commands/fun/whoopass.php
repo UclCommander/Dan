b6c1374ece4c467a92047abc89bcd8ed53dae983
UclCommander <luaeditor94@gmail.com>
2016-04-07 16:13:38 -0500

    Closes #39

--- addons/commands/fun/whoopass.php
+++ addons/commands/fun/whoopass.php
@@ -13,19 +13,5 @@ use Dan\Irc\Location\User;
 command(['whoopass'])
     ->helpText('When a normal beating just won\'t do!')
     ->handler(function (Connection $connection, User $user, $message, Channel $channel) {
-        $data = explode(' ', $message);
-
-        if ($data[0] && $user->hasPermissionTo('kick')) {
-            if (!$channel->getUser($connection->user)->hasPermissionTo('kick')) {
-                $user->notice('I\'m not allowed to kick users here.');
-
-                return;
-            }
-
-            $channel->kick($data[0], 'When a normal beating just won\'t do! WHOOPASS! Extra strength! http://skycld.co/whoopass');
-
-            return;
-        }
-
-        $channel->message('When a normal beating just won\'t do! http://skycld.co/whoopass');
+        $channel->message("When a normal beating just won't do! http://skycld.co/whoopass");
     });
