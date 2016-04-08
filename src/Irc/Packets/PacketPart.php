e467709b7445b6baba0e5f0dfc745a5c91cd209d
UclCommander <luaeditor94@gmail.com>
2016-04-08 15:48:22 -0500

    Fix error thrown when the bot parts a channel.

--- src/Irc/Packets/PacketPart.php
+++ src/Irc/Packets/PacketPart.php
@@ -15,18 +15,19 @@ class PacketPart extends Packet
      */
     public function handle(array $from, array $data)
     {
+        $this->triggerEvent('irc.part', [
+            'user'          => new User($this->connection, ...$from),
+            'channel'       => $this->connection->getChannel($data[0]),
+            'connection'    => $this->connection,
+        ]);
+
+
         if ($from[0] != $this->connection->user->nick) {
             $channel = $this->connection->getChannel($data[0]);
             $channel->removeUser($from[0]);
         } else {
             $this->connection->removeChannel($data[0]);
         }
-
-        $this->triggerEvent('irc.part', [
-            'user'          => new User($this->connection, ...$from),
-            'channel'       => $this->connection->getChannel($data[0]),
-            'connection'    => $this->connection,
-        ]);
 
         if (!config('dan.debug')) {
             console()->message("[<magenta>{$this->connection->getName()}</magenta>] <yellow>{$from[0]}</yellow> <cyan>left {$data[0]}</cyan>");
