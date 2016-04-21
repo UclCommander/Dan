00738ae62c6560526f4b1b85328ca17919c530eb
UclCommander <luaeditor94@gmail.com>
2016-04-21 15:04:45 -0500

    Convert command

---
+++ addons/commands/utility/convert.php
@@ -0,0 +1,22 @@
+<?php
+
+use Dan\Irc\Location\Channel;
+use Dan\Support\Web;
+use Illuminate\Support\Collection;
+
+command(['convert'])
+    ->helpText("Converts something to something else using DuckDuckGo")
+    ->func(function(Channel $channel, $message) {
+
+        $query = urlencode("convert {$message}");
+        $request = Web::json("http://api.duckduckgo.com/?q={$query}&format=json&pretty=1");
+
+        if (empty($request) || !is_array($request['Answer']))  {
+            $channel->message("No conversion results.");
+
+            return;
+        }
+
+        $answerData = $request['Answer']['data'];
+        $channel->message("[ <yellow>{$answerData['markup_input']} {$answerData['left_unit']}</yellow> is <cyan>{$answerData['raw_answer']} {$answerData['right_unit']}</cyan> ]");
+    });
