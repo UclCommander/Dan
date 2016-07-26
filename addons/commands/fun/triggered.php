1ea92570e513d2f638988edf4f1a431527c13da0
UclCommander <luaeditor94@gmail.com>
2016-07-26 16:01:23 -0500

    Add triggered

---
+++ addons/commands/fun/triggered.php
@@ -0,0 +1,13 @@
+<?php
+
+use Dan\Irc\Location\Channel;
+use Dan\Irc\Location\User;
+
+command(['triggered'])
+    ->allowPrivate()
+    ->helpText("I HaS bEn TRIggERED FRoM ReADIng tHIS TExT OF HeLPerNEsS")
+    ->handler(function (User $user, $message, Channel $channel = null) {
+        $location = $channel ?? $user;
+
+        $location->message('http://i0.kym-cdn.com/photos/images/newsfeed/001/053/925/9d8.gif');
+    });
